6a26ff30c1f96a5e252a91da8b7dc074b61fd2db
Anson Guo <anson.guo.921104@gmail.com>
2023-01-30 17:20:28 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2
 
-// Write your code here
+let random
+random = Math.ceil(Math.random())
+
+
+const num3 = 24
+const num4 = 5
+const mod = num3 % num4
+
+let max = Math.max(1,2,3,20)
+
+
+
+
+
+
